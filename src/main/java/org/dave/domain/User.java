f979fbd244df17758d0c078d44c102dd4f0f8a2e
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:14:57 -0400

    Added fields and a constructor in User class

--- src/main/java/org/dave/domain/User.java
+++ src/main/java/org/dave/domain/User.java
@@ -1,4 +1,13 @@
 package org.dave.domain;
 
 public abstract class User {
+    protected String id;
+    protected String name;
+
+    private static int nextId = 1;
+
+    public User(String id, String name) {
+        this.id = String.format("%05d", nextId++);
+        this.name = name;
+    }
 }
