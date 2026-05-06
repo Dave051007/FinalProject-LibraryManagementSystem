74d837b9eed22a815ff9064bea6f1430d5d72705
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:30:15 -0400

    Implement Reportable interface to Admin class for borrowing reports

--- src/main/java/org/dave/domain/Admin.java
+++ src/main/java/org/dave/domain/Admin.java
@@ -1,8 +1,13 @@
 package org.dave.domain;
 
-public class Admin extends User {
+public class Admin extends User implements Reportable {
 
     public Admin(String name) {
         super(name);
+    }
+
+    @Override
+    public String report() {
+        return "";
     }
 }
---
+++ src/main/java/org/dave/domain/Reportable.java
@@ -0,0 +1,5 @@
+package org.dave.domain;
+
+public interface Reportable {
+    String report();
+}
