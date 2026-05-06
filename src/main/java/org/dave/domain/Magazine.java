6f4d593a7e1f55b85792460b7ee7b2756e8e9a67
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:44:47 -0400

    Added a constructor for Magazine subclass

--- src/main/java/org/dave/domain/Magazine.java
+++ src/main/java/org/dave/domain/Magazine.java
@@ -4,4 +4,9 @@ public class Magazine extends Item {
     private int issueNumber;
     private int publisher;
 
+    public Magazine(String title, Status status, int issueNumber, int publisher) {
+        super(title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
