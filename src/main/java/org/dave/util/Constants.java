137a4d80930173fc9b2c7cbf2af4d760fe263470
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:54:17 -0400

    Added two constant values in Constants class: MAX_BORROWABLE_BOOKS_STUDENTS and MAX_BORROWABLE_ITEMS_TEACHERS

--- src/main/java/org/dave/util/Constants.java
+++ src/main/java/org/dave/util/Constants.java
@@ -1,4 +1,6 @@
 package org.dave.util;
 
 public class Constants {
+    public static final int MAX_BORROWABLE_BOOKS_STUDENTS = 5;
+    public static final int MAX_BORROWABLE_ITEMS_TEACHERS = 10;
 }
