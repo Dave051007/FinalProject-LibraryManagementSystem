ad5c2de7463c784bfed731566ddc0dbd8d9a4f2b
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:42:49 -0400

    Added an Item class constructor

--- src/main/java/org/dave/domain/DVD.java
+++ src/main/java/org/dave/domain/DVD.java
@@ -3,4 +3,10 @@ package org.dave.domain;
 public class DVD extends Item {
     private String director;
     private int duration;
+
+    public DVD(String title, Status status, String director, int duration) {
+        super(title, status);
+        this.director = director;
+        this.duration = duration;
+    }
 }
