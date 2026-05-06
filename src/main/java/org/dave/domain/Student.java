aea7f3417a720b2d55e7ce3516a84cd76a81838a
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-06 18:24:00 -0400

    Added a Student class constructor

--- src/main/java/org/dave/domain/Student.java
+++ src/main/java/org/dave/domain/Student.java
@@ -1,7 +1,13 @@
 package org.dave.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student extends User {
     private List<Item> borrowedItems;
+
+    public Student(String name) {
+        super(name);
+        this.borrowedItems = new ArrayList<>(borrowedItems);
+    }
 }
