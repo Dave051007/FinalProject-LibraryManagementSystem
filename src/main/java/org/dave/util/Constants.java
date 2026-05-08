4dc67f8c95f4594e0f1859a2e9547082b7b4b787
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-08 17:25:03 -0400

    Added Getters and Setters for Constant class fields

--- src/main/java/org/dave/util/Constants.java
+++ src/main/java/org/dave/util/Constants.java
@@ -1,5 +1,10 @@
 package org.dave.util;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Constants {
     public static final int MAX_BORROWABLE_BOOKS_STUDENTS = 5;
     public static final int MAX_BORROWABLE_ITEMS_TEACHERS = 10;
