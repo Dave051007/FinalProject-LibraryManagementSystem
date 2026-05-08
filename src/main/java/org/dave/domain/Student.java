355f868d8165b89ce59fe22e2dbc6a99ca34bda8
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-08 17:26:06 -0400

    Added borrow method in Student class

--- src/main/java/org/dave/domain/Student.java
+++ src/main/java/org/dave/domain/Student.java
@@ -1,5 +1,7 @@
 package org.dave.domain;
 
+import org.dave.util.Constants;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,5 +11,29 @@ public class Student extends User {
     public Student(String name) {
         super(name);
         this.borrowedItems = new ArrayList<>();
+    }
+
+    /**
+     * Adds a book in Student's borrowedItems list.
+     * If item is not a book, or if Student's exceeds borrow limits,
+     * throw an exception
+     * @param item the book to be added
+     * @return true if added the book successfully,
+     * false if borrowed book is already in borrowedItems list
+     */
+    @Override
+    public boolean borrow(Item item) {
+        if (!(item instanceof Book)) {
+            throw new IllegalArgumentException("Item is not Book");
+        } else if (borrowedItems.size() == Constants.MAX_BORROWABLE_BOOKS_STUDENTS) {
+            throw new IllegalArgumentException("Too many borrowed items");
+        }
+
+        if (borrowedItems.contains(item)) {
+            return false;
+        }
+
+        borrowedItems.add(item);
+        return true;
     }
 }
