3a8d6e8136a8b959594fae6f993d1f17656f93e6
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-12 21:21:14 -0400

    Updated User constructor and field

--- src/main/java/org/dave/domain/User.java
+++ src/main/java/org/dave/domain/User.java
@@ -3,13 +3,14 @@ package org.dave.domain;
 public abstract class User {
     protected String id;
     protected String name;
-    protected String associatedLibrary;
+    protected Library associatedLibrary;
 
     private static int nextId = 1;
 
-    public User(String name) {
+    public User(String name, Library associatedLibrary) {
         this.id = String.format("%05d", nextId++);
         this.name = name;
+        this.associatedLibrary = associatedLibrary;
     }
 
 }
