e64299bcfee9aa3a1defa8bce2266eb380012652
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-12 20:18:43 -0400

    Added hasItem method in Library to check if item is present in the Library

--- src/main/java/org/dave/domain/Library.java
+++ src/main/java/org/dave/domain/Library.java
@@ -7,4 +7,13 @@ import java.util.List;
 public class Library {
     private List<Item> items;
     private List<Item> members;
+
+    /**
+     * Check if item is inside List of items
+     * @param item the item
+     * @return true if item is inside of items list
+     */
+    public boolean hasItem(Item item) {
+        return items.contains(item);
+    }
 }
