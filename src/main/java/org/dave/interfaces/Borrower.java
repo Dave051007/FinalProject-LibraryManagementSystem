a5b699ba8677ca62d409ffbb88d75c361c410b78
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-12 20:21:45 -0400

    Added additional parameter in borrowItem and returnItem method, must now indicate library name

--- src/main/java/org/dave/interfaces/Borrower.java
+++ src/main/java/org/dave/interfaces/Borrower.java
@@ -1,8 +1,9 @@
 package org.dave.interfaces;
 
 import org.dave.domain.Item;
+import org.dave.domain.Library;
 
 public interface Borrower {
-    boolean borrowItem(Item item);
-    boolean returnItem(Item item);
+    boolean borrowItem(Item item, Library library);
+    boolean returnItem(Item item, Library library);
 }
