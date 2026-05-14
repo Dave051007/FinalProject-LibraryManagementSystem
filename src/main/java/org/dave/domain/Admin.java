87372f72015b8b268737bd462e6ac761f124ea83
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-14 12:52:33 -0400

    Added report method in Admin class

--- src/main/java/org/dave/domain/Admin.java
+++ src/main/java/org/dave/domain/Admin.java
@@ -2,6 +2,11 @@ package org.dave.domain;
 
 import org.dave.interfaces.Reportable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Admin extends User implements Reportable {
 
     public Admin(String name, Library associatedLibrary) {
@@ -10,7 +15,14 @@ public class Admin extends User implements Reportable {
     }
 
     @Override
-    public String report() {
-        return "";
+    public Map<Item.Status, List<String>> report() {
+       Map<Item.Status, List<String>> result = new TreeMap();
+
+        for (Item item : associatedLibrary.getItems()) {
+            result.putIfAbsent(item.getStatus(), new ArrayList<>());
+            result.get(item.getStatus()).add(item.getTitle());
+        }
+
+       return result;
     }
 }
--- src/main/java/org/dave/interfaces/Reportable.java
+++ src/main/java/org/dave/interfaces/Reportable.java
@@ -1,5 +1,10 @@
 package org.dave.interfaces;
 
+import org.dave.domain.Item;
+
+import java.util.List;
+import java.util.Map;
+
 public interface Reportable {
-    String report();
+    Map<Item.Status, List<String>> report();
 }
