019f59ff32cd509b7051da4ac2b15ee3c8a7f86b
Dave <git@github.com:Dave051007/FinalProject---SchoolSystem.git>
2026-05-14 16:30:27 -0400

    Added file paths of Book, Dvd, Magazine csv

--- src/main/java/org/dave/util/Constants.java
+++ src/main/java/org/dave/util/Constants.java
@@ -9,4 +9,7 @@ public class Constants {
     public static final int MAX_BORROWABLE_BOOKS_STUDENTS = 5;
     public static final int MAX_BORROWABLE_ITEMS_TEACHERS = 10;
     public static final String USER_CSV_PATH = "src/main/resources/user.csv";
+    public static final String BOOK_CSV_PATH = "src/main/resources/book.csv";
+    public static final String DVD_CSV_PATH = "src/main/resources/dvd.csv";
+    public static final String MAGAZINE_CSV_PATH = "src/main/resources/magazine.csv";
 }
